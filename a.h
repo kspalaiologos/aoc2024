effeaa19214a86f512b0a1beca09ad1f95918965
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-01 13:27:26 +0100

    refactor
    type-generic sort in a.h; simplify d01.c

--- a.h
+++ a.h
@@ -29,11 +29,20 @@ typedef double D;
 #define Fx(n,a...) for(int x=0;x<n;x++){a;}
 #define Fy(n,a...) for(int y=0;y<n;y++){a;}
 #define F_(z,i,n,a...) for(int z=i;z<n;z++){a;}
+#define F$(x,y,z,a...) for(x;y;z){a;}
 #define F1(n,x,a...) for(i32 i=0;i<n;i+=x){a;}
 #define I(x,a...) if(x){a;}
 #define J(a...) else I(a)
 #define E(a...) else{a;}
 #define SWAP(a,b) {typeof(a) t = a; a = b; b = t;}
+
+// Type-generic insertion sort.
+// E.g. L a[10]; isort(a, 10, y < x); sorts descending.
+#define isort(v, n, c...) \
+  { L j; typeof(v[0]) y, x; \
+    Fi(n, y = v[i], j = i; \
+      W(j > 0 && ({ x = v[j - 1]; c; })) \
+        v[j] = v[j - 1], j--; v[j] = y)}
 
 S FILE * fget(C * n, C * m) {
   FILE * f = fopen(n, m);
--- d01.c
+++ d01.c
@@ -1,24 +1,17 @@
 #include"a.h"
 #define P 1000
-V sort(L * a, I n) {
-  Fi(n, F_(j, i + 1, n, I(a[i] <= a[j], SWAP(a[i], a[j]))))
-}
 I main(V){
   FILE * f = fget("d01a.txt", "r");
-  L n[2][P], s = 0, sim = 0;
+  L n[2][P], s = 0, sim = 0, a;
   Fi(P, scanl(f, &n[0][i]) && scanl(f, &n[1][i]));
   struct { L k, v; } h[P]; Fi(P, h[i].k = -1, h[i].v = 0);
   Fi(P,
-    L a = n[1][i] % P;
-    W(h[a].k != -1 && h[a].k != n[1][i])
-      a = (a + 1) % P;
+    F$(a=n[1][i]%P,h[a].k!=-1&&h[a].k!=n[1][i],a=(a+1)%P)
     h[a].k = n[1][i], h[a].v++)
   Fi(P,
-    L x = n[0][i], v, a = x % P;
-    W(h[a].k != -1 && h[a].k != x)
-      a = (a + 1) % P;
-    I(v = h[a].v, sim += x * v))
-  sort(n[0], P); sort(n[1], P);
+    F$(a=n[0][i]%P,h[a].k!=-1&&h[a].k!=n[0][i],a=(a+1)%P)
+    I(h[a].v, sim += n[0][i] * h[a].v))
+  isort(n[0], P, y < x); isort(n[1], P, y < x);
   Fi(P, s += labs(n[0][i] - n[1][i]));
   printf("T1: %ld, T2: %ld\n", s, sim);
 }
