c9eab01003a87e572a43f98148b290c1efbb5649
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-06 07:25:04 +0100

    d06: cache strlen(g[pcx])

--- d06.c
+++ d06.c
@@ -1,8 +1,9 @@
 #include"a.h"
-C g[140][140], v[140][140], v2[140][140], d; I n, p1, p2, cx, cy;
+C g[140][140], v[140][140], v2[140][140], d;
+I n, p1, p2, cx, cy, l[140];
 I s() {
   I pcx = cx + dir4cw[d][0], pcy = cy + dir4cw[d][1];
-  I(0 <= pcx && pcx < n && 0 <= pcy && pcy < strlen(g[pcx]),
+  I(0 <= pcx && pcx < n && 0 <= pcy && pcy < l[pcx],
     I(g[pcx][pcy] == '#', d = (d + 1) % 4)
     E(I(v[pcx][pcy] == d + 1, p2++; R 0)
       E(v[cx = pcx][cy = pcy] = d + 1)) R 1) E(R 0) }
@@ -10,7 +11,7 @@ V a()_(
   Fi(140, Fj(140, I(g[i][j] == '^', cx = i, cy = j)))
   bzero(v, 19600); v[cx][cy] = 1; d = 0; W(s()))
 M(Fp f = fget("d06.txt", "r");
-  W(fgets(g[n], 140, f), g[n][strlen(g[n]) - 1] = 0, n++)
+  W(fgets(g[n], 140, f), l[n] = strlen(g[n]) - 1, n++)
   a(); memcpy(v2, v, 19600); Fi(140, Fj(140, p1 += v[i][j] != 0))
   Fi(140, Fj(140, I(g[i][j] == '.' && v2[i][j], g[i][j] = '#'; a(); g[i][j] = '.')))
   resi(p1, p2))
