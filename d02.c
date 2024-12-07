bb990e911dc0376308196a3913e569c462f47568
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-08 00:45:47 +0100

    d02 refactor with fall/fany

--- d02.c
+++ d02.c
@@ -1,6 +1,6 @@
 #include"a.h" // 479 531
 M(Fp f = fget("d02.txt", "r");
-  I z = 0, n, d, t1 = 0, t2 = 0, c;
+  I z = 0, n, d, t1 = 0, t2 = 0, c, s[16];
   C l[64], * p; I in[1000][16], x[16], xl[1000];
   W(p = fgets(l, 64, f),
     F$(n = 0, *p, p++, in[z][n++] = strtol(p, &p, 10))
@@ -8,8 +8,7 @@ M(Fp f = fget("d02.txt", "r");
   Fi(z, Fj(15, c = -1;
     #define D(f, n) f(x[n] - x[n - 1])
     Fy(xl[i], I(y != j - 1, x[++c] = in[i][y]))
-    d = D(sgn, 1);
-    Fy(c, d = d == D(sgn, y + 1) ? d : 0)
-    Fy(c, d = d && C3(1,<=,D(abs, y + 1),<=,3))
-    I(d, I(!j, t1++) E(t2++; break))))
+    Fy(c, s[y] = D(sgn, y + 1)) s[c] = s[c - 1];
+    I(Fall(y, 1, c+1, s[y] == s[y - 1] && C3(1,<=,D(abs, y),<=,3)),
+      I(!j, t1++) E(t2++; break))))
   resi(t1, t2))
