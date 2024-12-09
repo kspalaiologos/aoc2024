ebb0bb5cf98b7ee3a5e42d3632a8cc9863315db9
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-09 13:28:33 +0100

    d05

--- d05.c
+++ d05.c
@@ -1,13 +1,14 @@
 #include"a.h"
-I r0[1200], r1[1200], nr, u[999][99], nu[999], g[100][200], ui;
+#define P 100
+I r0[1200], r1[1200], nr, u[999][P], nu[999], g[P][2*P], ui;
 I uidx(I v)_(Fj(nu[ui], I(u[ui][j] == v, R j)) -1)
 I uhas(I v)_(uidx(r0[v]) != -1 && uidx(r1[v]) != -1)
 I ru(V)_(
-  I ng[100], id[100], pr[100], t[100], ti = 0, q[100], qh = 0, qt = 0, n;
-  Fi(100, id[i] = ng[i] = pr[i] = 0)
+  I ng[P], id[P], pr[P], t[P], ti = 0, q[P], qh = 0, qt = 0, n;
+  Fi(P, id[i] = ng[i] = pr[i] = 0)
   Fi(nr, I(uhas(i),
     pr[r0[i]] = pr[r1[i]] = 1; id[g[r0[i]][ng[r0[i]]++] = r1[i]]++))
-  Fi(100, I(!id[i] && pr[i], q[qt++] = i))
+  Fi(P, I(!id[i] && pr[i], q[qt++] = i))
   W(qh < qt, t[ti++] = n = q[qh++];
     Fj(ng[n], I(!--id[g[n][j]], q[qt++] = g[n][j]))) t[ti / 2])
 M(C * p = slurp("d05.txt");
