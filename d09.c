9e5376c0913ef965bfde65bd2db8a892042bca5f
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-11 14:34:50 +0100

    cheap shot at fixing d09 perf (manual unroll)

--- d09.c
+++ d09.c
@@ -1,14 +1,15 @@
 #include "a.h" // 6344673854800 6360363199987
 #define P 19999
-V p2(T * e, I sz)_(Fid(sz, I(e[i] != -1 && i, I n = e[i], need = 1, has;
-  W (e[--i] == n && i, need++); Fj(i, I(e[j] == -1,
-    F$(has = 1, e[++j] == -1, has++) I(need <= has,
-      F$(j -= has + 1, need--, e[i] = -1, e[++j] = e[++i]) B))) i++)))
-V p1(T * e, I sz)_(I j = sz - 1; Fi(j, I(e[i] == -1,
-  W(e[j] == -1, j--) SWAP(e[i], e[j]) j--)))
+#define U I(e[++j], B) /* Manual Unroll */
+V p2(T * e, I sz)_(Fid(sz, I(e[i] != 0 && i, I n = e[i], need = 1, has;
+  W (e[--i] == n && i, need++); Fj(i, I(e[j] == 0,
+    has = j; W(1, U U U U U U U U) has = j - has; I(need <= has,
+      F$(j -= has + 1, need--, e[i] = 0, e[++j] = e[++i]) B))) i++)))
+V p1(T * e, I sz)_(I j = sz - 1; Fi(j, I(e[i] == 0,
+  W(e[j] == 0, j--) SWAP(e[i], e[j]) j--)))
 M(C * c = slurp("d09.txt"); I ns = Fisum(P, 0, c[i] - '0');
-  T e[2][P*5]; I j = 0, k = 0; L s[2];
+  T e[2][P*5]; I j = 1, k = 0; L s[2];
   Fi(P, Fx(c[i] - '0', e[0][k] = j; k++)
-    I(++i < P, Fx(c[i] - '0', e[0][k] = -1; k++) j++))
+    I(++i < P, Fx(c[i] - '0', e[0][k] = 0; k++) j++))
   CM(e[1], e[0]); p1(e[0], ns); p2(e[1], ns);
-  Fx(2, s[x] = Fisum(ns, 0l, MAX(i * e[x][i], 0))) resl(s[0], s[1]))
+  Fx(2, s[x] = Fisum(ns, 0l, MAX(i * (e[x][i] - 1), 0))) resl(s[0], s[1]))
