0b7bab1ae26aae2f4d7c0546440e1b6195b77314
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-11 16:31:13 +0100

    d09 refactor

--- d09.c
+++ d09.c
@@ -3,8 +3,8 @@
 #define U I(e[++j], B) /* Manual Unroll */
 V p2(T * e, I sz)_(Fid(sz, I(e[i] && i, I n = e[i], need = 1;
   W (e[--i] == n && i, need++); Fj(i, I(!e[j],
-    I has = j; W(1, U U U U U U U U) has = j - has; I(need <= has,
-      F$(j -= has + 1, need--, e[i] = 0, e[++j] = e[++i]) B))) i++)))
+    I has = j; W(1, U U U U) I(need <= j - has,
+      F$(j = has - 1, need--, e[i] = 0, e[++j] = e[++i]) B))) i++)))
 V p1(T * e, I sz)_(I j = --sz; Fi(j, I(!e[i], W(!e[j--]) SWAP(e[i], e[j+1]))))
 M(C * c = slurp("d09.txt"); I ns = Fisum(P, 0, c[i] - '0');
   T e[2][P*5]; ZM(e); I j = 1, k = 0; L s[2];
