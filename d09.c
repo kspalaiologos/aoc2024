67de4e1c03d6437b0425057023d70bc7e9e34d20
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-12 15:57:17 +0100

    bullshit

--- d09.c
+++ d09.c
@@ -2,7 +2,7 @@
 #define P 19999
 #define U I(e[++j], B) /* Manual Unroll */
 V p2(T * e, I sz)_(Fid(sz, I(e[i] && i, I n = e[i], need = 1, has;
-  W (e[--i] == n && i, need++); Fj(i, I(!e[j],
+  W (e[--i] == n && i, need++); Fj(i, W(j < i && e[j++]); I(!e[--j],
     has = j; W(1, U U U U) I(need <= j - has,
       F$(j = has - 1, need--, e[i] = 0, e[++j] = e[++i]) B))) i++)))
 V p1(T * e, I sz)_(I j = --sz; Fi(j, I(!e[i], W(!e[j--]) SWAP(e[i], e[j+1]))))
