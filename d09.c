5ea1db6f133a1062ed19f2495359c97b18907ea5
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-12 16:20:48 +0100

    add an optimisation to d09 spotted by dzaima

--- d09.c
+++ d09.c
@@ -1,10 +1,9 @@
-#include "a.h" // 6344673854800 6360363199987
+#include "a.h" // 6344673854800 6360363199987 / O(n) spotted by @dzaima
 #define P 19999
-#define U I(e[++j], B) /* Manual Unroll */
-V p2(T * e, I sz)_(Fid(sz, I(e[i] && i, I n = e[i], need = 1, has;
-  W (e[--i] == n && i, need++); Fj(i, W(j < i && e[j++]); I(!e[--j],
-    has = j; W(1, U U U U) I(need <= j - has,
-      F$(j = has - 1, need--, e[i] = 0, e[++j] = e[++i]) B))) i++)))
+V p2(T * e, I sz)_(I s = 0; Fid(sz, I(e[i] && i, I n = e[i], need = 1, has;
+  W (e[--i] == n && i, need++); C f=1; F_(j,s,i, I(!e[j], I(f, s=j; f=0)
+    has = j; W(!e[++j]) has = j - has; I(need <= has,
+      F$(j -= has + 1, need--, e[i] = 0, e[++j] = e[++i]) B))) i++)))
 V p1(T * e, I sz)_(I j = --sz; Fi(j, I(!e[i], W(!e[j--]) SWAP(e[i], e[j+1]))))
 M(C * c = slurp("d09.txt"); I ns = Fisum(P, 0, c[i] - '0');
   T e[2][P*5]; ZM(e); I j = 1, k = 0; L s[2];
