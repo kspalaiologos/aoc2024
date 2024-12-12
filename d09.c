52a9321b31d36e86185d76705f27561b208371a1
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-12 08:13:07 +0100

    tentative d12

--- d09.c
+++ d09.c
@@ -1,9 +1,9 @@
 #include "a.h" // 6344673854800 6360363199987
 #define P 19999
 #define U I(e[++j], B) /* Manual Unroll */
-V p2(T * e, I sz)_(Fid(sz, I(e[i] && i, I n = e[i], need = 1;
+V p2(T * e, I sz)_(Fid(sz, I(e[i] && i, I n = e[i], need = 1, has;
   W (e[--i] == n && i, need++); Fj(i, I(!e[j],
-    I has = j; W(1, U U U U) I(need <= j - has,
+    has = j; W(1, U U U U) I(need <= j - has,
       F$(j = has - 1, need--, e[i] = 0, e[++j] = e[++i]) B))) i++)))
 V p1(T * e, I sz)_(I j = --sz; Fi(j, I(!e[i], W(!e[j--]) SWAP(e[i], e[j+1]))))
 M(C * c = slurp("d09.txt"); I ns = Fisum(P, 0, c[i] - '0');
