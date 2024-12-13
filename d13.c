713a988d3a0b8279420f993806b4e4242121f70d
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-13 07:00:43 +0100

    simplify

--- d13.c
+++ d13.c
@@ -4,6 +4,6 @@
 L f(L a[6], I p)_(I(p, a[4] += 1e13; a[5] += 1e13)
   L m = D(0,3,1,2), c = D(4,3,5,2), d = D(5,0,4,1);
   I(c % m || d % m || c / m < 0 || d / m < 0, R 0); (3 * c + d) / m)
-L s[2], i, j, a[P][6]; M(Fp fp = fget("d13.txt"); LINESF(99, fp, W(*p,
-  I(isdigit(*p++), a[i][j++] = strtol(p-1, &p, 10))) I(j == 6, i++, j = 0))
+L s[2], i, a[P][6]; M(Fp fp = fget("d13.txt"); LINESF(99, fp, W(*p,
+  I(isdigit(*p++), a[i/6][i++%6] = strtol(p-1, &p, 10))))
   Fi(P, Fx(2, s[x] += f(a[i], x))) resl(s[0], s[1]))
