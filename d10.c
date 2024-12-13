7ffe675981866b794bc22a0a36dc0e3e98c88fc0
Kamila Szewczyk <kspalaiologos@gmail.com>
2024-12-13 13:10:38 +0100

    d10/d12

--- d10.c
+++ d10.c
@@ -1,9 +1,9 @@
 #include"a.h"
 #define P 60
 I d, p1, p2; C g[P][P], r9[P][P], v[P][P];
-V f(I r, I c){
-  I h = g[r][c], nc, nr; I(h == 9, p2++; r9[r][c] = 1; R) v[r][c] = 1;
-  Fi(4, I(CR0((nr = r + dir4cw[i][0]), d) && CR0((nc = c + dir4cw[i][1]), d)
-    && g[nr][nc] == h + 1 && !v[nr][nc], f(nr, nc))) v[r][c] = 0;}
+V f(v2I c){
+  v2I n; I h = g[V2I(c)]; I(h == 9, p2++; r9[V2I(c)] = 1; R) v[V2I(c)] = 1;
+  Fi(4, n = c + dir4cw[i]; I(CR0(n[0], d) && CR0(n[1], d)
+    && g[V2I(n)] == h + 1 && !v[V2I(n)], f(n))) v[V2I(c)] = 0;}
 M(Fp h = fget("d10.txt"); W(fgets(g[d], P, h), d++); F2d(d, g[i][j] -= '0')
-  F2d(d, I(!g[i][j], ZM(r9); f(i, j); F2d(P,0, p1 += r9[i][j]))) resi(p1, p2))
+  F2d(d, I(!g[i][j], ZM(r9); f(ij); F2d(P,0, p1 += r9[i][j]))) resi(p1, p2))
--- d12.c
+++ d12.c
@@ -1,4 +1,4 @@
-#include"a.h"
+#include"a.h" // TODO: Use vector APIs.
 #define P 140
 #define Fs(a,b) \
   !Fany(j,0,s, k1[j] == y - a && k2[j] == z - b && k3[j] == x)
